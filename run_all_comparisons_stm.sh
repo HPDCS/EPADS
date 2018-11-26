806d804519b8eec5b6988dbc83da63797bba395b
Your Name <you@example.com>
2018-11-26 01:06:17 +0100

    minor

--- run_all_comparisons_stm.sh
+++ run_all_comparisons_stm.sh
@@ -25,7 +25,7 @@ do
 			numactl --physcpubind=+0-$(( $THREADS-1 )) stamp/vacation/./vacation -n4 -q60 -u90 -r1048576 -t40194304 -c$THREADS     
 			python hope_config_writer.py -commits_round 25000
 			numactl --physcpubind=+0-$(( $THREADS-1 )) stamp/intruder/./intruder -a10 -l512 -n642144 -s1 -t$THREADS
-			python hope_config_writer.py -commits_round 3000
+			python hope_config_writer.py -commits_round 1750
 			numactl --physcpubind=+0-$(( $THREADS-1 )) stamp/genome/./genome -g22238 -s1024 -n247772160 -t$THREADS
 			python hope_config_writer.py -commits_round 100000
 			numactl --physcpubind=+0-$(( $THREADS-1 )) stamp/ssca2/./ssca2 -s23 -i1.0 -u1.0 -l3 -p3 -t$THREADS
