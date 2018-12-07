6260e81d4057c8d078b5d5575e1a8f75df11c47a
Your Name <you@example.com>
2018-12-07 16:38:41 +0100

    Fixed stm comparison tests

--- run_all_comparisons_stm.sh
+++ run_all_comparisons_stm.sh
@@ -27,8 +27,8 @@ do
 			numactl --physcpubind=+0-$(( $THREADS-1 )) stamp/intruder/./intruder -a5 -l512 -n642144 -s1 -t$THREADS
 			python hope_config_writer.py -commits_round 1750
 			numactl --physcpubind=+0-$(( $THREADS-1 )) stamp/genome/./genome -g22238 -s1024 -n247772160 -t$THREADS
-			python hope_config_writer.py -commits_round 100000
-			numactl --physcpubind=+0-$(( $THREADS-1 )) stamp/ssca2/./ssca2 -s23 -i1.0 -u1.0 -l3 -p3 -t$THREADS
+			python hope_config_writer.py -commits_round 75000
+			numactl --physcpubind=+0-$(( $THREADS-1 )) stamp/ssca2/./ssca2 -s24 -i1.0 -u1.0 -l3 -p3 -t$THREADS
 		done
 	done
 done 
